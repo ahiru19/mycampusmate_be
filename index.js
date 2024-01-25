7e636b8eaabab726f9dd135dc950e87b38884c5e
Sam Mark <sam.manga@mobilemoney.ph>
2024-01-25 15:17:30 +0800

    added new funcrtions and models.

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.use(express.json()); // This is for the body to be recognized
 app.use(cookieParser());
 app.use(cors())
 const { appRoutes } = require("./main_routes"); //This is the file for the whole part of your Routes
+const fileupload = require("express-fileupload");//This is for the files to be recognized
+
+app.use(fileupload({ //this is to use the file and give size of the file
+    createParentPath: true,
+    abortOnLimit: true,
+    limitHandler: (req, res, next) => {
+        res.status(413).send({ status: "Limit Reached", message: "Upload file limit reached, check your file size" });
+    },
+    limits: { fileSize: 10 * 1024 * 1024 },
+}));
 
 //register all routes
 appRoutes.forEach((route) => {
