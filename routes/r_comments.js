f297df929950184f1e7e7077db1a264fffe4ebac
Sam Mark <sam.manga@mobilemoney.ph>
2024-02-05 19:41:21 +0800

    added comments.

---
+++ routes/r_comments.js
@@ -0,0 +1,19 @@
+const comments = require("../controller/c_comments");
+
+module.exports.routeComments = [
+    {
+        path: "/comments/addComment/",
+        method: "post",
+        action: comments.addComments
+    },
+    {
+        path: "/comments/getAll/",
+        method: "get",
+        action: comments.getClient
+    },
+    {
+        path: "/comments/getAll/",
+        method: "get",
+        action: comments.getClient
+    },
+]
